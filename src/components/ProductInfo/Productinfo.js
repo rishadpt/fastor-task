79f3ab87cbbc28a1c81f14676ebb32953b20c204
rishadpt <rishadpt16@gmail.com>
2022-08-04 20:38:41 +0530

    draggable added

--- src/components/ProductInfo/Productinfo.js
+++ src/components/ProductInfo/Productinfo.js
@@ -4,6 +4,7 @@ import { fastorServices } from '../../utils/service'
 import { Link, useParams } from 'react-router-dom'
 import { IoIosArrowBack } from 'react-icons/io'
 import { AiFillHeart, AiFillStar, AiOutlineShareAlt } from 'react-icons/ai'
+import { MdEdit } from 'react-icons/md'
 import Loader from '../Loader/Loader'
 
 export default function Productinfo() {
@@ -16,6 +17,9 @@ export default function Productinfo() {
   const thirdref = useRef('')
   const [wishlist, setwishlist] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [x, setX] = useState()
+  const [y, setY] = useState()
+  const [edit, setEdit] = useState(false)
 
   const wishlistActive = (action) => {
     if (action === 'add') {
@@ -41,19 +45,27 @@ export default function Productinfo() {
       firstref.current.classList.remove('slider__active')
       thirdref.current.classList.remove('slider__active')
     }
-   else if (action === 3) {
+    else if (action === 3) {
       thirdref.current.classList.add('slider__active')
       firstref.current.classList.remove('slider__active')
       secondref.current.classList.remove('slider__active')
     }
   }
 
+  const getPosition = (e) => {
+
+    const rect = e.target.getBoundingClientRect()
+    console.log(rect)
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+    console.log(e)
     
-
-
-
-
+    setX(x)
+    setY(y)
+  }
+ 
   useEffect(() => {
+    console.log(edit)
     fastorServices.getRestaurants().then((data) => {
 
       let filter = data.filter((item) => item.restaurant_id === id)
@@ -62,6 +74,7 @@ export default function Productinfo() {
       setLoading(false)
     })
   }, [])
+
   const handleOnClick = () => {
     if (navigator.share) {
       navigator
@@ -78,50 +91,53 @@ export default function Productinfo() {
     }
   };
 
-  console.log(image)
+
   return (
-    <div className="productinfo-container">
-      <div className="image-header" >
-        <Link to='/home'>
-          <IoIosArrowBack />
-        </Link>
+    loading ? <Loader /> :
+      <div className="productinfo-container">
+        <div className="image-header" >
+         {!edit  && <Link to='/home'>
+            <IoIosArrowBack />
+          </Link>}
 
-        <div className="share_container">
-          <AiOutlineShareAlt onClick={handleOnClick} />
-        <div className="share" ref={wishlistRef}>
-          <AiFillHeart onClick={(e) => { wishlist ? wishlistActive('remove') : wishlistActive('add') }} />
+          <div className="share_container">
+            <MdEdit onClick={() => {edit ? setEdit(false) : setEdit(true) }} />
+           {!edit  && <AiOutlineShareAlt onClick={handleOnClick} />}
+
+           {!edit  && <div className="share" ref={wishlistRef}>
+              <AiFillHeart onClick={(e) => { wishlist ? wishlistActive('remove') : wishlistActive('add') }} />
+            </div>}
+          </div>
+
         </div>
+        {image[0] ? image.map((item, index) => (
+          <div  onMouseMove={edit ? getPosition :''} id={index} key={index} style={{ backgroundImage: ` url(${item.url})` }} className="walpapper-container">
+            <img style={{ top: edit ? `${x}px` : x, left: edit ? `${y}px` : y }} src='/Images/logo.png' alt="" />
+
+          </div>)) : <Loader />}
+        <div className="arrows">
+          <div ref={firstref} onClick={() => { slider(1) }} className="slider__active slider__dot"></div>
+          <div ref={secondref} onClick={() => { slider(2) }} className="slider__dot " ></div>
+          <div onClick={() => { slider(3) }} ref={thirdref} className="slider__dot "></div>
+        </div>
+        <div className="product__info">
+          <div className="title__price">
+            <h1>{data.restaurant_name}</h1>
+            <p>$ {data.avg_cost_for_two}</p>
+          </div>
+          <p>
+            Cake pastry,Pastas, Connaught place,New delhi
+          </p>
+          <span>
+            350g
+          </span>
+          <div className="star_container">
+            {Array(5).fill(0).map((index) => (
+              <AiFillStar style={{ color: `${index < data?.rating?.restaurant_avg_rating ? '#FFC833' : null}` }} />))}
+
+          </div>
         </div>
 
       </div>
-      {image[0] ? image.map((item, index) => (
-        <div id={index} key={index} style={{ backgroundImage: ` url(${item.url})` }} className="walpapper-container">
-          <img src='/Images/logo.png' alt=""  />
-
-        </div>)):<Loader />}
-      <div className="arrows">
-        <div ref={firstref} onClick={() => { slider(1) }} className="slider__active slider__dot"></div>
-        <div ref={secondref} onClick={() => { slider(2) }} className="slider__dot " ></div>
-        <div onClick={() => { slider(3) }} ref={thirdref} className="slider__dot "></div>
-      </div>
-      <div className="product__info">
-        <div className="title__price">
-          <h1>{data.restaurant_name}</h1>
-          <p>$ {data.avg_cost_for_two}</p>
-        </div>
-        <p>
-          Cake pastry,Pastas, Connaught place,New delhi
-        </p>
-        <span>
-          350g
-        </span>
-        <div className="star_container">
-          {Array(5).fill(0).map((index) => (
-            <AiFillStar style={{ color: `${index < data?.rating?.restaurant_avg_rating ? '#FFC833' : null}` }} />))}
-
-        </div>
-      </div>
-
-    </div>
   )
 }
